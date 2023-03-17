31786acb70b9859536a35c15cc4aa2a5cd792388
zzoofe.dev <zzoofe.dev@gmail.com>
2023-03-17 16:19:44 +0400

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 import lazyLoading from './modules/lazyLoading';
 import mask from "./components/mask";
-import modal from "./components/modal";
 
 lazyLoading.init();
 mask.init();
-modal.init();
 
 
